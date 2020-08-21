9a050527ad14bc6dbdbd0ec9bddf0a55a890b92d
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-21 09:55:13 -0300

    gigasecond shorter version

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,6 +1,1 @@
-export const gigasecond = (date) => {
-  const date_in_miliseconds = date.getTime();
-  const gigasecond_in_miliseconds = 1e12;
-  const sum = date_in_miliseconds + gigasecond_in_miliseconds;
-  return new Date(sum);
-};
+export const gigasecond = date => new Date(date.getTime() + 1e12);
