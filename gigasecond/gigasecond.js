c9d8afe8aba313260b861b5db42a5525e5f2dfcd
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-21 10:03:30 -0300

    comments for gigaseconds

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,1 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getTime
+// JavaScript uses milliseconds as the unit of measurement, whereas Unix Time is in seconds.
+// 1e12 = you have 1e9 (1 with 9 zeroes for gigaseconds) plus 3 zeroes (due to millisecond conversion)
 export const gigasecond = date => new Date(date.getTime() + 1e12);
