4fa0d95efd31ef2c38134efb3641b7a3b5dbf73b
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-21 14:06:57 -0300

    color duo: changing parseInt to Number

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,7 +1,7 @@
 export const colorCode = (name) => COLORS.indexOf(name);
 
 export const decodedValue = (list) => {
-  return parseInt(`${colorCode(list[0])}${colorCode(list[1])}`)
+  return Number(`${colorCode(list[0])}${colorCode(list[1])}`)
 };
 
 export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
