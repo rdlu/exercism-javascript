cd674783861c2ddbadbe770041826964eb780706
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-08-21 14:59:49 -0300

    rna, after replace with function

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,10 +1,10 @@
-export const mapping = {
-  'G': 'C',
-  'C': 'G',
-  'T': 'A',
-  'A': 'U'
-}
+export const toRna = str => {
+  const mapping = {
+    'C': 'G',
+    'G': 'C',
+    'A': 'U',
+    'T': 'A'
+  };
 
-export const toRna = (str) => {
-  return str.split('').map(i => mapping[i]).join('')
+  return str.replace(/[CGAT]/g, item => mapping[item]);
 };
