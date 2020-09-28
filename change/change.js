1c669b9b2f9c6fb5ceabf58bc17a84d990c1c28a
Rodrigo Dlugokenski <rddweb@gmail.com>
2020-09-28 17:02:33 -0300

    coin change using combinations

--- change/change.js
+++ change/change.js
@@ -1,10 +1,23 @@
-//
-// This is only a SKELETON file for the 'Change' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Change {
-  calculate(coinArray, target) {
-    throw new Error("Remove this statement and implement this function");
-  }
+    calculate(coins, target) {
+        if (target === 0) return []
+        if (target < 0) throw new Error('Negative amount for change')
+        if (!coins.length) return null
+
+        for (let i = 1; i <= Math.trunc(target / Math.min(...coins)); i++) {
+            for (let candidate of this.combinations_with_repetition(coins, i)) {
+                if (candidate.reduce((a, b) => a + b, 0) === target) return candidate
+            }
+        }
+
+        throw new Error('No change found')
+    }
+
+    combinations_with_repetition(lst, n) {
+        return n ? (
+            lst.length ? this.combinations_with_repetition(lst, n - 1).map(function (t) {
+                return [lst[0]].concat(t);
+            }).concat(this.combinations_with_repetition(lst.slice(1), n)) : []
+        ) : [[]];
+    }
 }
