3a95bec02ff0f9551331c5f0c90823abc4fe2d83
Rodrigo Dlugokenski <rddweb@gmail.com>
2020-09-29 10:28:05 -0300

    coin change dynamic programming

--- change/change.js
+++ change/change.js
@@ -1,51 +1,41 @@
 export class Change {
     calculate(coins, target) {
-        if (target === 0) return []
-        if (target < 0) throw new Error('Negative amount for change')
-        if (!coins.length) return null
+        if (target < 0) throw new Error('Negative amount for change');
 
-        for (let i = 1; i <= Math.trunc(target / Math.min(...coins)); i++) {
-            for (let candidate of this.combinations_with_repetition_2(coins, i)) {
-                if (candidate.reduce((a, b) => a + b, 0) === target) return candidate.sort((a, b) => a - b)
+        if (coins.includes(target)) return [target];
+
+        const minNumberOfCoinsFor = new Array(target + 1);
+        const firstCoinFor = new Array(target + 1);
+
+        minNumberOfCoinsFor[0] = 0;
+
+        coins.forEach((faceValue) => {
+            for (let i = faceValue; i <= target; i += 1) {
+                const priorIndex = i - faceValue;
+                const subProblemCoinCount = minNumberOfCoinsFor[priorIndex];
+
+                if (subProblemCoinCount === undefined) continue;
+
+                if (subProblemCoinCount + 1 < minNumberOfCoinsFor[i] || minNumberOfCoinsFor[i] === undefined) {
+                    minNumberOfCoinsFor[i] = subProblemCoinCount + 1;
+                    firstCoinFor[i] = faceValue;
+                }
             }
+        });
+
+        if (minNumberOfCoinsFor[target] === undefined) {
+            throw new Error("No change found");
         }
 
-        throw new Error('No change found')
-    }
+        let total = target;
+        const change = [];
 
-    // some JS libraries call it baseN, other people call it k-selection or k-multiset
-    // using js-combinatorics, genratorics library, etc is taking much more time for some weird reason
-    combinations_with_repetition(lst, n) {
-        return n ? (
-            lst.length ? this.combinations_with_repetition(lst, n - 1).map(function (t) {
-                return [lst[0]].concat(t);
-            }).concat(this.combinations_with_repetition(lst.slice(1), n)) : []
-        ) : [[]];
-    }
+        while (total > 0) {
+            const coin = firstCoinFor[total];
+            change.push(coin);
+            total -= coin;
+        }
 
-    // from https://rosettacode.org/wiki/Combinations_with_repetitions#ES6
-    // faster than my version above
-    combinations_with_repetition_2(xs, k) {
-        const concatMap = (f, xs) => [].concat.apply([], xs.map(f));
-        const head = xs => xs.length ? xs[0] : undefined;
-        const isNull = xs => (xs instanceof Array) ? xs.length < 1 : undefined;
-        const map = (f, xs) => xs.map(f);
-        const pure = x => [x];
-        const dropWhile = (p, xs) => {
-            let i = 0;
-            for (let lng = xs.length;
-                 (i < lng) && p(xs[i]); i++) {}
-            return xs.slice(i);
-        };
-        const comb = (n, ys) => {
-            if (0 === n) return ys;
-            if (isNull(ys)) return comb(n - 1, map(pure, xs));
-
-            return comb(n - 1, concatMap(zs => {
-                const h = head(zs);
-                return map(x => [x].concat(zs), dropWhile(x => x !== h, xs));
-            }, ys));
-        };
-        return comb(k, []);
+        return change.sort((a, b) => a - b);
     }
 }
