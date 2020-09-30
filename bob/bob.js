44813dd686b36de55fb46f3fd9085e0ce0917fc4
Rodrigo Dlugokenski <rddweb@gmail.com>
2020-09-30 16:13:49 -0300

    bob solution

--- bob/bob.js
+++ bob/bob.js
@@ -1,3 +1,22 @@
-export const hey = (message) => {
-  throw new Error("Remove this statement and implement this function");
-};
+export function response(message) {
+  const q = message.trim()
+  if (isQuestion(q) && isYell(q)) return "Calm down, I know what I'm doing!"
+  if (isQuestion(q)) return "Sure."
+  if (isYell(q)) return "Whoa, chill out!"
+  if (isSilence(q)) return "Fine. Be that way!"
+  return "Whatever."
+}
+
+function isQuestion(q) {
+  return q.slice(-1) === '?'
+}
+
+function isYell(q) {
+  return q === q.toUpperCase() && (q.slice(-1) === '!' || isNaN(Number(q.slice(-2, -1))))
+}
+
+function isSilence(q) {
+  return q === ''
+}
+
+export const hey = response
