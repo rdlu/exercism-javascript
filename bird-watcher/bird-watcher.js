04a895d50fb351653a89f2db5904d2b7c5d9159a
Rodrigo Dlugokenski <git@dlu.one>
2023-10-10 17:05:19 -0300

    bird-watcher: more mutable wrangling

---
+++ bird-watcher/bird-watcher.js
@@ -0,0 +1,38 @@
+// @ts-check
+/**
+ * Calculates the total bird count.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number} total bird count
+ */
+export function totalBirdCount(birdsPerDay) {
+  return birdsPerDay.reduce((prev, cur) => { return prev + cur; });
+}
+
+/**
+ * Calculates the total number of birds seen in a specific week.
+ *
+ * @param {number[]} birdsPerDay
+ * @param {number} week
+ * @returns {number} birds counted in the given week
+ */
+export function birdsInWeek(birdsPerDay, week) {
+  let startIdx = (week - 1) * 7;
+  return totalBirdCount(birdsPerDay.slice(startIdx, week * 7));
+}
+
+/**
+ * Fixes the counting mistake by increasing the bird count
+ * by one for every second day.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number[]} corrected bird count data
+ */
+export function fixBirdCountLog(birdsPerDay) {
+  for (let index = 0; index < birdsPerDay.length; index++) {
+    if(index % 2 === 0) {
+      birdsPerDay[index] += 1;
+    }
+  }
+   return birdsPerDay;
+}
