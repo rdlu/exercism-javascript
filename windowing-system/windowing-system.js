baea9ec0e26a2e0bd64c3d03b7122e478ad25170
Rodrigo Dlugokenski <git@dlu.one>
2023-10-16 14:32:26 -0300

    js classes with some improved maths

--- windowing-system/windowing-system.js
+++ windowing-system/windowing-system.js
@@ -30,28 +30,20 @@ export class ProgramWindow {
         this.position = new Position();
     }
 
+    limit(value, min, max) {
+       return Math.min(Math.max(value, min), max);
+    }
+
     resize(size) {
-        let w = size.width < 1 ? 1 : size.width;
-        let h = size.height < 1 ? 1 : size.height;
-
-        let limit_h = this.screenSize.height - this.position.y;
-        let limit_w = this.screenSize.width - this.position.x;
-
-        w = Math.min(limit_w, w);
-        h = Math.min(limit_h, h);
+        let w = this.limit(size.width, 1, this.screenSize.width - this.position.x);
+        let h = this.limit(size.height, 1, this.screenSize.height - this.position.y);
 
         this.size.resize(w, h);
     }
 
     move(position) {
-        let x = Math.max(0, position.x);
-        let y = Math.max(0, position.y);
-
-        let limit_x = this.screenSize.width - this.size.width;
-        let limit_y = this.screenSize.height - this.size.height;
-
-        x = Math.min(x, limit_x);
-        y = Math.min(y, limit_y);
+        let x = this.limit(position.x, 0, this.screenSize.width - this.size.width);
+        let y = this.limit(position.y, 0, this.screenSize.height - this.size.height);
 
         this.position.move(x, y);
     }
