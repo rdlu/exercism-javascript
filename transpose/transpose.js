08ed982d06b9968f4ee69aa66daa27870dce329a
Rodrigo Dlugokenski <git@dlu.one>
2023-12-01 17:26:23 -0300

    transpose with js, better args

--- transpose/transpose.js
+++ transpose/transpose.js
@@ -1,9 +1,9 @@
 export const transpose = (lines) => {
   const maxWidth = Math.max(0, ...lines.map((x) => x.length));
   // reduce from right just to check the last char and put spaces on the left if needed
-  const reverseConcat = (_, i) => lines.reduceRight((acc, row) => (row[i] || (acc ? ' ' : '')) + acc, '');
+  const reverseConcat = (rows) => (_, i) => rows.reduceRight((acc, row) => (row[i] || (acc ? ' ' : '')) + acc, '');
   // `from` is more sane than destruct
-  return Array.from({ length: maxWidth }, reverseConcat)
+  return Array.from({ length: maxWidth }, reverseConcat(lines))
 };
 
 
