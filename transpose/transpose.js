9203961f993906e085232886006c7d78f6a1cf28
Rodrigo Dlugokenski <git@dlu.one>
2023-12-01 17:04:54 -0300

    transpose with js

--- transpose/transpose.js
+++ transpose/transpose.js
@@ -1,9 +1,9 @@
 export const transpose = (lines) => {
   const maxWidth = Math.max(0, ...lines.map((x) => x.length));
-  // initializing an array of width with undef... js being js
-  const emptyArray = [...Array(maxWidth)];
   // reduce from right just to check the last char and put spaces on the left if needed
-  return emptyArray.map((_, i) => lines.reduceRight((acc, row) => (row[i] || (acc ? ' ' : '')) + acc, ''));
+  const reverseConcat = (_, i) => lines.reduceRight((acc, row) => (row[i] || (acc ? ' ' : '')) + acc, '');
+  // `from` is more sane than destruct
+  return Array.from({ length: maxWidth }, reverseConcat)
 };
 
 
