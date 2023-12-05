5ff6dbc40e03b99c6bd9a1d17237db293da5a721
Rodrigo Dlugokenski <git@dlu.one>
2023-12-05 09:53:54 -0300

    second triangle version

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -1,20 +1,26 @@
 export class Triangle {
   constructor(...sides) {
     this.sides = sides;
-    this.isTriangle = this.sides.every(side => side > 0);
+    this.uniqueSides = new Set(sides);
   }
 
   get isEquilateral() {
-    return this.isTriangle && this.sides.every(side => side === this.sides[0]);
+    return this.isValid() && this.uniqueSides.size === 1;
   }
 
   get isIsosceles() {
-    return this.isTriangle &&
-      this.sides[0] === this.sides[1] || this.sides[1] === this.sides[2] || this.sides[0] === this.sides[2];
+    return this.isValid() && (this.uniqueSides.size === 1 || this.uniqueSides.size === 2);
   }
 
   get isScalene() {
-    return this.isTriangle &&
-      this.sides[0] !== this.sides[1] && this.sides[1] !== this.sides[2] && this.sides[0] !== this.sides[2];
+    return this.isValid() && this.uniqueSides.size === 3;
+  }
+
+  isValid() {
+    let [x, y, z] = [...this.sides];
+    const validMinLength = this.sides.every(v => v > 0);
+    const validSides = ((x + y >= z) && (y + z >= x) && (x + z >= y));
+
+    return validMinLength && validSides;
   }
 }
